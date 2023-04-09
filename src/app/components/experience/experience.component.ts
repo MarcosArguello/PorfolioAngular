236d9d104eb61e6a4375e32277d95262f4030c07
MarcosArguello <arguellomarcos@gmail.com>
2023-04-09 03:29:02 -0300

    Listas

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/service/porfolio.service';
 
 @Component({
   selector: 'app-experience',
   templateUrl: './experience.component.html',
   styleUrls: ['./experience.component.css']
 })
-export class ExperienceComponent {
+export class ExperienceComponent implements OnInit{
+
+  experienciaList:any;
+
+  constructor(private datosPorfolio:PorfolioService){
+
+  }
+
+  ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data=>{
+      this.experienciaList=data.experience;
+    })
+  }
 
 }
