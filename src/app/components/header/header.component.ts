ad59b55423e9833900c5563127f7844d07deca41
MarcosArguello <arguellomarcos@gmail.com>
2023-04-09 02:10:03 -0300

    Base de datos

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/service/porfolio.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit{
+
+  miPorfolio:any;
+
+  constructor(private datosPorfolio:PorfolioService){
+
+  }
+
+  ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data =>{
+      console.log(data);
+      this.miPorfolio=data;
+    });
+  }
 
 }
