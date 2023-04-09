d2d8c82bf7a69b2f49bf18fe2970a90ea9530143
MarcosArguello <arguellomarcos@gmail.com>
2023-04-09 12:28:55 -0300

    Habilidades

--- src/app/components/skills/skills.component.ts
+++ src/app/components/skills/skills.component.ts
@@ -1,10 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PorfolioService } from 'src/app/service/porfolio.service';
 
 @Component({
   selector: 'app-skills',
   templateUrl: './skills.component.html',
   styleUrls: ['./skills.component.css']
 })
-export class SkillsComponent {
+export class SkillsComponent implements OnInit{
+
+  
+  skillAreaList:any;
+
+
+  constructor(private datosPorfolio:PorfolioService){
+
+  }
+
+  ngOnInit(): void {
+    this.datosPorfolio.obtenerDatos().subscribe(data=>{
+      this.skillAreaList=data.skills;
+    })
+  }
 
 }
